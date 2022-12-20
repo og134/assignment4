60d106bb211637414105fdfd324ab1b3485bce9d
omer gonen <omergonen13@gmail.com>
2022-12-21 01:27:23 +0200

    BinaryNumber.java working till 3.6 not included need to upload to vpl

--- src/BinaryNumber.java
+++ src/BinaryNumber.java
@@ -34,9 +34,38 @@ public class BinaryNumber implements Comparable<BinaryNumber>{
 
     //----------write your code BELOW this line only!!!---------------------------------------------------------
 
+    //--------------------helpers--------------------------------------------------------------------------------
+    public BinaryNumber(BitList num){
+        this.bits = new BitList(num);
+    }
+    public void compareSizes(BinaryNumber B){
+       this.bits.padding(B.bits.size());
+       B.bits.padding(this.bits.size());
+    }
+    public void compareSizes(BitList B){
+        this.bits.padding(B.size());
+        B.padding(this.bits.size());
+    }
+    //-------------------end of helpers------------------------------------------------------------------------
+
     //=========================== Intro2CS 2022/3, ASSIGNMENT 4, TASK 3.1 ================================================
+    public static int toInt(char c) {
+        return "0123456789".indexOf(c);
+    }
     public BinaryNumber(char c) {
-    	throw new UnsupportedOperationException("Delete this line and implement the method.");
+        this.bits = new BitList();
+        String binary = Integer.toBinaryString(toInt(c));
+        for(int i = binary.length() - 1;i>=0;i--){
+            this.bits.addLast(new Bit(binary.charAt(i) - '0'));
+        }
+        //add zero if there is one at the beginning
+        Bit LastBit = this.bits.getLast();
+        if(LastBit.toInt() == 1){
+            this.bits.addLast(new Bit(0));
+        }
+        else{
+            this.bits.addLast(LastBit);
+        }
     }
 
     //=========================== Intro2CS 2022/3, ASSIGNMENT 4, TASK 3.2 ================================================
@@ -44,23 +73,72 @@ public class BinaryNumber implements Comparable<BinaryNumber>{
         // Do not remove or change the next two lines
         if (!isLegal()) // Do not change this line
             throw new RuntimeException("I am illegal.");// Do not change this line
-        //
-        throw new UnsupportedOperationException("Delete this line and implement the method.");
+
+        String ans = "";
+        for (Bit bit : this.bits)
+            ans += bit.toString();
+        return ans;
     }
 
     //=========================== Intro2CS 2022/3, ASSIGNMENT 4, TASK 3.3 ================================================
     public boolean equals(Object other) {
-    	throw new UnsupportedOperationException("Delete this line and implement the method.");
+        if(! (other instanceof BinaryNumber))
+            return false;
+
+    	Iterator<Bit> ThisIter = this.bits.iterator();
+        Iterator<Bit> OtherIter =((BinaryNumber) other).bits.iterator();
+
+        if(ThisIter.hasNext() & OtherIter.hasNext()){
+            if(ThisIter.next().toInt() != OtherIter.next().toInt()){
+                return false;
+            }
+        }
+        //they are the same until the point one of them end
+        // 001 0011111111 for example
+        return ThisIter.hasNext() == OtherIter.hasNext();
     }
 
     //=========================== Intro2CS 2022/3, ASSIGNMENT 4, TASK 3.4 ================================================
     public BinaryNumber add(BinaryNumber addMe) {
-    	throw new UnsupportedOperationException("Delete this line and implement the method.");
+        Bit A;
+        Bit B ;
+        Bit Cin = new Bit(0);
+        compareSizes(addMe);
+        BitList res = new BitList();
+        BitList copyBits;
+        copyBits = this.bits;
+        while (addMe.bits.size() > 0){
+            A = this.bits.removeLast();
+            B = addMe.bits.removeLast();
+            res.addLast(Bit.fullAdderSum(A,B,Cin));
+            Cin = Bit.fullAdderCarry(A,B,Cin);
+        }
+        this.bits = copyBits;
+        res.reduce();
+        return new BinaryNumber(res);
     }
 
     //=========================== Intro2CS 2022/3, ASSIGNMENT 4, TASK 3.5 ================================================
     public BinaryNumber negate() {
-    	throw new UnsupportedOperationException("Delete this line and implement the method.");
+        this.bits.reduce();
+        BitList negBits = new BitList();
+        Iterator<Bit> thisIter = this.bits.iterator();
+        Bit temp;
+        while(thisIter.hasNext()) {
+            temp = thisIter.next().negate();
+            negBits.addLast(temp.negate());
+        }
+
+        //represent the number 1
+        //and then we will combine
+        BitList adder = new BitList();
+        adder.addLast(new Bit(1));
+        adder.addLast(new Bit(0));
+        adder.padding(negBits.size());
+        BinaryNumber test =  new BinaryNumber(adder);
+        BinaryNumber res = new BinaryNumber(add(test));
+
+        return res;
     }
 
     //=========================== Intro2CS 2022/3, ASSIGNMENT 4, TASK 3.6 ================================================
